fdc0a5a737ca540a0863528720fa472d320abe6f
Collin Myers <collin_myers@outlook.com>
2024-03-19 21:11:29 -0400

    Started delete account edge function, can delete docs, but not account yet

---
+++ Functions/DeleteUserAndData/src/main.js
@@ -0,0 +1,63 @@
+import { Account, Client, Databases, Query } from "node-appwrite";
+
+const client = new Client();
+
+client
+  .setEndpoint("https://mypi.bd.psu.edu/v1") // Your API Endpoint
+  .setProject("653a90dd1993aebe707f") // Your project ID
+  .setKey("f5b4297a913e131ba4a11344b1047c08d7e7ba02dc91ff0c0ae7828e0c9d4143080e3fd7c60674abf0b78d6bdbb4cd214eaa9e2a6084030080387e9a358d14f8bbb61eda2922792c222d444ba7d458f32b7cc6758a07b2c195d01ee132236262255a02121d7e2ff189041065e0cf652e9c327dae3f8ba5c798ded51177578da1")
+;
+const databases = new Databases(client);
+
+const getUserNotificationDocuments = async (userID) => {
+  try {
+    const result = await databases.listDocuments(
+      "653ae4b2740b9f0a5139", // databaseId
+      "65d651f3af4d612b0b75", // collectionId
+      [Query.equal("UID", [userID])] // queries (optional)
+    );
+    return result;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+const DeleteUserNotificationDocuments = async (userDocuments) => {
+  try {
+    const deletePromises = userDocuments.documents.map(async doc => {
+      await databases.deleteDocument(
+        "653ae4b2740b9f0a5139", // databaseId
+        "65d651f3af4d612b0b75", // collectionId
+        doc.$id // documentId
+      );
+      console.log(`Document ${doc.$id} deleted`);
+    });
+    await Promise.all(deletePromises);
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+const deleteAccount = async (userID) => {
+  const account = new Account(client);
+  try {
+    const result = await account.deleteIdentity(
+      userID // identityId
+    );
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+export default async function main() {
+
+  const userID = "65fa33a4ab28e6e8d233"
+
+  const userDocuments = await getUserNotificationDocuments(userID);
+
+  const deleteUserDocuments = await DeleteUserNotificationDocuments(userDocuments);
+
+  const deleteUser = await deleteAccount(userID);
+};
+
+main();
