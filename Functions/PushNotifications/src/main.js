63a27451ae74d7bf75aea6fcee46d8e3155a55ee
ericpetika <ejp5559@psu.edu>
2024-04-13 18:37:03 -0400

    Collin's update for push

--- Functions/PushNotifications/src/main.js
+++ Functions/PushNotifications/src/main.js
@@ -7,85 +7,81 @@ async function getTokens() {
     const PAGE_SIZE = 25;
 
     let allTokens = [];
-    let finalAllTokens = [];
 
     let offset = 0;
     let response;
-    do {
-        response = await database.listDocuments(
-            '653ae4b2740b9f0a5139',
-            '65d651f3af4d612b0b75',
-            [Query.limit(PAGE_SIZE), Query.offset(offset)]
-        );
+    try {
+        do {
+            response = await database.listDocuments(
+                '653ae4b2740b9f0a5139',
+                '65d651f3af4d612b0b75',
+                [Query.limit(PAGE_SIZE), Query.offset(offset)]
+            );
 
-        allTokens = [...allTokens, ...response.documents];
-        offset += PAGE_SIZE;
-    } while (response.documents.length > 0);
-
-    let iterator = 0;
-
-    allTokens.forEach(document => {
-        finalAllTokens[iterator] = document['ExpoPushToken'];
-        iterator++;
-    });
-
-
-    return finalAllTokens;
+            allTokens = [...allTokens, ...response.documents];
+            offset += PAGE_SIZE;
+        } while (response.documents.length > 0);
+        return allTokens.map(document => document['ExpoPushToken']);
+    } catch (error) {
+        return error;
+    }
 }
 
 // Function to send push notification using Expo's API
 async function sendPushNotification(expoPushTokens, notifTitle, notifBody) {
+    try {
 
-    // Create a new Expo SDK client
-    let expo = new Expo();
+        // Create a new Expo SDK client
+        let expo = new Expo();
 
-    // Create the messages array
-    let messages = [];
-    for (let token of expoPushTokens) {
-        // Check if each token is a valid push token
-        if (!Expo.isExpoPushToken(token)) {
-            console.error(`Push token ${token} is not a valid Expo push token`);
-            continue;
-        }
-
-        // Create the message to send
-        messages.push({
-            to: token,
-            sound: "default",
-            title: notifTitle,
-            body: notifBody,
-        });
-    }
-
-    // Send the notifications
-    let chunks = expo.chunkPushNotifications(messages);
-    let tickets = [];
-    (async () => {
-        for (let chunk of chunks) {
-            try {
-                let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
-                tickets.push(...ticketChunk);
-            } catch (error) {
-                console.error(error);
+        // Create the messages array
+        let messages = [];
+        for (let token of expoPushTokens) {
+            // Check if each token is a valid push token
+            if (!Expo.isExpoPushToken(token)) {
+                console.error(`Push token ${token} is not a valid Expo push token`);
+                continue;
             }
+
+            // Create the message to send
+            messages.push({
+                to: token,
+                sound: "default",
+                title: notifTitle,
+                body: notifBody,
+            });
         }
-    })();
-    return;
+
+        // Send the notifications
+        let chunks = expo.chunkPushNotifications(messages);
+        let tickets = [];
+        (async () => {
+            for (let chunk of chunks) {
+                try {
+                    let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+                    tickets.push(...ticketChunk);
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        })();
+        return;
+    } catch (error) {
+        return error;
+    }
 }
 
 // Main function entry point
-export default async function main({ req, res }) {
-
-
+export default async function main({ req, res, log, error }) {
     const title = req.headers.title;
     const body = req.headers.body;
-    const expoPushTokens = await getTokens();
 
     try {
-        const result = await sendPushNotification(expoPushTokens, title, body);
+        const expoPushTokens = await getTokens();
+        await sendPushNotification(expoPushTokens, title, body);
         return res.json({ success: true });
-    } catch (error) {
-        console.error('Error sending push notification:', error);
+    } catch (err) {
+        error('Error sending push notification:', err);
         return res.send({ success: false });
     }
 }
