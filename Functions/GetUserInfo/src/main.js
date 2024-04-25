49569e0b9ef22b6a856bd36219ce64ce1968b094
Collin Myers <collin_myers@outlook.com>
2024-04-25 15:19:00 -0400

    Updated getUser info to just pull all users w/ no filtering

--- Functions/GetUserInfo/src/main.js
+++ Functions/GetUserInfo/src/main.js
@@ -22,65 +22,27 @@ const getEmailUsers = async (server) => {
     offset += loopResponse.users.length;
     usersSoFar += loopResponse.users.length;
   }
-  const authenticatedUsers = usersList.filter(user => user.email !== "");
-  return authenticatedUsers;
-}
-
-const filterUserInfo = (authenticatedUsers) => {
-  return authenticatedUsers.map(({ $id, name, email, labels }) => ({ $id, name, email, labels }))
-}
-const validateUser = async (JWT, userID) => {
-  try {
-    const client = new Client();
-    client
-      .setEndpoint("https://mypi.bd.psu.edu/v1") //Appwrite Endpoint
-      .setProject("653a90dd1993aebe707f") // Appwrite Project ID
-      .setJWT(JWT)
-      ;
-    const account = new Account(client);
-    const getAccountInfo = await account.get();
-    if (getAccountInfo.$id === userID && getAccountInfo.labels.includes("ManageUsers")) {
-      return true;
-    } else {
-      return "User ID provided does not match ID in JWT or user does not have the correct permissions";
-    }
-  } catch (err) {
-    return err;
-  }
+  return usersList;
 }
 
 export default async function main({ req, res, log, error }) {
-  const JWT = req.headers["x-appwrite-user-jwt"];
-  const userID = req.headers["x-appwrite-user-id"];
-
   try {
-    const isUser = await validateUser(JWT, userID);
+    const server = new Client();
+    server
+      .setEndpoint("https://mypi.bd.psu.edu/v1") // Your API Endpoint
+      .setProject("653a90dd1993aebe707f") // Your project ID
+      .setKey("b1259e652349c7f220aed6aa75b3f1f157c2aa58b224488525757ce3e7496791d69b77b80ce9903db94ddb7fcc75eff978fe8ef5f884cc2c60c33df62f9dafb7ce2c9d5b6095dcddeb42fff0e8455a6f077e7993f3fabb324e899b9221e238fcd5f51ef9dad5320202d129b44ea5eb145d4cd53db2ed3721f9f4a0cd990960e4") // Your secret API key
+      ;
 
-    if (isUser !== true) {
-      error("Error:");
-      error(isUser);
-      return res.send("Not Authorized", 403);
-    } else {
-      const server = new Client();
+    const allUsers = await getEmailUsers(server);
 
-      server
-        .setEndpoint("https://mypi.bd.psu.edu/v1") // Your API Endpoint
-        .setProject("653a90dd1993aebe707f") // Your project ID
-        .setKey("b1259e652349c7f220aed6aa75b3f1f157c2aa58b224488525757ce3e7496791d69b77b80ce9903db94ddb7fcc75eff978fe8ef5f884cc2c60c33df62f9dafb7ce2c9d5b6095dcddeb42fff0e8455a6f077e7993f3fabb324e899b9221e238fcd5f51ef9dad5320202d129b44ea5eb145d4cd53db2ed3721f9f4a0cd990960e4") // Your secret API key
-        ;
-
-      const authenticatedUsers = await getEmailUsers(server);
-      const userInfo = filterUserInfo(authenticatedUsers);
-      console.log(userInfo.length);
-
-      if (req.method === "GET") {
-        return res.json({ userInfo });
-      }
-
+    if (req.method === "GET") {
+      return res.json({ allUsers });
     }
+
   } catch (error) {
-    console.error(error)
-    error("Error during account retrieval:", err);
+    error(error)
+    error("Error during account retrieval:", error);
     return res.json({ success: false });
   }
 }
